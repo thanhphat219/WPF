c5f23e04818746b6a1754b3bbf760f9418b4f47a
thanhphat219 <phat01283249260@gmail.com>
2021-01-25 22:59:07 +0700

    B3 + B4

---
+++ WPF/B3/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace B3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            //var range = (Hyperlink)txbl1.Inlines.ElementAt(0);
+            //txbl2.Text = range.NavigateUri.ToString();
+            txbl3.Inlines.Add(new Run("Hello World") { FontSize = 20 });
+
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(e.Uri.ToString());
+        }
+    }
+}
---
+++ WPF/B4/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace B4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Button button = new Button();
+            button.Click += Button_Click;
+            //button.Content = "Free Education";
+            gird.Children.Add(button);
+
+            //TextBlock textBlock = new TextBlock();
+            //textBlock.Text = "Share to be better";
+            //button.Content = textBlock;
+
+            TextBox textBox = new TextBox();
+            textBox.Height = 50;
+            textBox.Width = 100;
+            button.Content = textBox;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Click");
+        }
+    }
+}
