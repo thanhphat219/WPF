29e3d319249d6350be767f11ca8bbadb23fed231
thanhphat219 <phat01283249260@gmail.com>
2021-01-26 10:25:02 +0700

    B5

---
+++ WPF/B5/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace B5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValue_Click(object sender, RoutedEventArgs e)
+        {
+            txblValue.Text = txbValue.Text;
+        }
+        int DoubleValue(int value)
+        {
+            return value * 2;
+        }
+        private void txbValue_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int value = 0;
+            if (int.TryParse(txbValue.Text, out value))
+                txblValue.Text = DoubleValue(value).ToString();
+            else
+                txblValue.Text = "Vui lòng nhập giá trị chính xác";
+        }
+    }
+}
